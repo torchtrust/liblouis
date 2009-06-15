113e6d44ce8183f9b88174286564ca12d307c992
John Boyer <john.boyer@jjb-software.com>
2009-06-15 06:26:53 +0000

    Code to compile grouping opcode in branches/johnb-devel/liblouis/compileTranslationTable.c

--- liblouis/compileTranslationTable.c
+++ liblouis/compileTranslationTable.c
@@ -2341,6 +2341,63 @@ compileNumber (FileInfo * nested)
 }
 
 static int
+compileGrouping (FileInfo * nested)
+{
+  int k;
+  CharsString name;
+  CharsString groupChars;
+  CharsString groupDots;
+  CharsString dotsParsed;
+  TranslationTableCharacter *charsDotsPtr;
+  if (!getToken (nested, &name, "name operand"))
+    return 0;
+  if (!getRuleCharsText (nested, &groupChars))
+    return 0;
+  if (!getToken (nested, &groupDots, "dots operand"))
+    return 0;
+  for (k = 0; k < groupDots.length && groupDots.chars[k] != ','; k++);
+  if (k == groupDots.length)
+    {
+      compileError (nested,
+		    "Dots operand must consist of two cells separated by a comma");
+      return 0;
+    }
+  groupDots.chars[k] = '-';
+  if (!parseDots (nested, &dotsParsed, &groupDots))
+    return 0;
+  if (groupChars.length != 2 || dotsParsed.length != 2)
+    {
+      compileError (nested,
+"two Unicode characters and two cells separated by a comma are needed."
+);
+      return 0;
+    }
+  charsDotsPtr = addCharOrDots (nested, groupChars.chars[0], 0);
+  charsDotsPtr->attributes |= CTC_Math;
+  charsDotsPtr->uppercase = charsDotsPtr->realchar;
+  charsDotsPtr->lowercase = charsDotsPtr->realchar;
+  charsDotsPtr = addCharOrDots (nested, groupChars.chars[1], 0);
+  charsDotsPtr->attributes |= CTC_Math;
+  charsDotsPtr->uppercase = charsDotsPtr->realchar;
+  charsDotsPtr->lowercase = charsDotsPtr->realchar;
+  charsDotsPtr = addCharOrDots (nested, dotsParsed.chars[0], 1);
+  charsDotsPtr->attributes |= CTC_Math;
+  charsDotsPtr->uppercase = charsDotsPtr->realchar;
+  charsDotsPtr->lowercase = charsDotsPtr->realchar;
+  charsDotsPtr = addCharOrDots (nested, dotsParsed.chars[1], 1);
+  charsDotsPtr->attributes |= CTC_Math;
+  charsDotsPtr->uppercase = charsDotsPtr->realchar;
+  charsDotsPtr->lowercase = charsDotsPtr->realchar;
+  if (!addRule (nested, CTO_Grouping, &groupChars, &dotsParsed, 0, 0))
+    return 0;
+  if (!addRuleName (nested, &name))
+    return 0;
+  putCharAndDots (nested, groupChars.chars[0], dotsParsed.chars[0]);
+  putCharAndDots (nested, groupChars.chars[1], dotsParsed.chars[1]);
+  return 1;
+}
+
+static int
 compileUplow (FileInfo * nested)
 {
   int k;
@@ -3360,6 +3417,9 @@ doOpcode:
     case CTO_NoBreak:
       ok = compileNoBreak (nested);
       break;
+case CTO_Grouping:
+ok = compileGrouping (nested);
+break;
     case CTO_UpLow:
       ok = compileUplow (nested);
       break;
